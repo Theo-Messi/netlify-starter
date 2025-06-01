33e24387b4100600920155fe5cb7cf7fa99d5380
Theo <fanxiaobin422@gmail.com>
2025-06-01 12:44:27 +0800

    docs: update description setting

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
+const Bark = require('waline-notification-bark');
 
 const app = Waline({
+  plugins: [Bark],
   env: 'netlify',
   async postSave(comment) {
     // do what ever you want after save comment
